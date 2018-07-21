b64c02046d05ccca8287c94fe365517c69b8ba75
Raaj Raisinghani <raajhriday@gmail.com>
2018-07-22 01:53:46 +0530

    Fb added

--- index.php
+++ index.php
@@ -53,6 +53,10 @@
     <section class="py-5">
       <div class="container">
         <div class="row">
+            <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Our Suporters</h2>
+            <h3 class="section-subheading text-muted"></h3>
+          </div>
           <div class="col-md-3 col-sm-6">
             <a href="#">
               <img class="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt="">
